b310974dc19ab066f873380c83e08baae0f35e5d
Ofer Shapira <ofers@elementor.com>
2025-05-06 16:09:15 +0300

    fix name

--- js/devtools.js
+++ js/devtools.js
@@ -1,10 +1,10 @@
 // Create a panel in chrome DevTools
 chrome.devtools.panels.create(
-  "postMessage", // Panel title
+  "PostMessage", // Panel title
   "images/icon16.png", // Panel icon
   "panel.html", // Panel page
   (panel) => {
     // Panel created callback
-    console.log("postMessage DevTools panel created");
+    console.log("PostMessage DevTools panel created");
   }
 ); 
